e4911c0d4a8e3bda0d9a0ecdb4577d42dc6a92fa
N1neKitsune <88555694+N1neKitsune@users.noreply.github.com>
2023-11-29 17:32:30 +0100

    Add files via upload

---
+++ TerminalRicingArch.sh
@@ -0,0 +1,274 @@
+#!/bin/bash
+
+echo "Select the tools you want to install:"
+echo "1. YAY (Yet Another Yogurt - An AUR Helper)"
+echo "2. HUNT (A simplified Find command made with Rust)"
+echo "3. DOOIT (A todo manager)"
+echo "4. PLOW (An HTTP(S) benchmarking tool)"
+echo "5. CATP (Print the output of a running process)"
+echo "6. CURL-IMPERSONATE (A special build of curl)"
+echo "7. NTFY (Send push notifications to your phone or desktop)"
+echo "8. JQP (A TUI playground for exploring jq)"
+echo "9. K9S (Kubernetes CLI)"
+echo "10. NNN (A full-featured terminal file manager)"
+echo "11. NCDU (A disk utility for Unix systems)"
+echo "12. SSH-TOOLS (Making SSH more convenient)"
+echo "13. NALA (A front-end for libapt-pkg)"
+echo "14. NEOFETCH (A command-line system information tool)"
+echo "15. OTS (Share end-to-end encrypted secrets)"
+echo "16. BOTTOM (A graphical process/system monitor)"
+echo "17. VIDDY (A modern watch command)"
+echo "18. HTMLQ (Like jq, but for HTML)"
+echo "19. SYSZ (A terminal UI for systemctl)"
+echo "20. MILLER (Like awk, sed, cut, join, and sort for data formats)"
+echo "21. PUEUE (A command-line task management tool)"
+echo "22. VIZEX (Visualize disk space usage)"
+echo "23. GPING (Ping, but with a graph)"
+echo "24. TEMPMAIL (A temporary email right from your terminal)"
+echo "25. BAT (A cat clone with syntax highlighting)"
+echo "26. NAVI (An interactive cheatsheet tool)"
+echo "27. Install all"
+
+read -p "Enter your choice (e.g., 1 2 3): " choices
+
+# Function to install YAY
+install_yay() {
+    if ! command -v yay &> /dev/null; then
+        sudo pacman -Sy --needed git base-devel
+        git clone https://aur.archlinux.org/yay.git
+        cd yay && makepkg -si && cd .. && rm -rf yay
+    else
+        echo "YAY is already installed."
+    fi
+}
+
+# Function to install HUNT
+install_hunt() {
+    if ! command -v cargo &> /dev/null; then
+        sudo pacman -Sy --needed rust
+    fi
+    cargo install hunt
+}
+
+# Function to install DOOIT
+install_dooit() {
+    if ! command -v pip &> /dev/null; then
+        sudo pacman -Sy --needed python-pip
+    fi
+    pip install dooit || yay -S dooit-git
+}
+
+# Function to install PLOW
+install_plow() {
+    if ! command -v go &> /dev/null; then
+        sudo pacman -Sy --needed go
+    fi
+    go install github.com/six-ddc/plow@latest || yay -S plow
+}
+
+# Function to install CATP
+install_catp() {
+    wget https://github.com/rapiz1/catp/releases/download/v0.2.0/catp-x86_64-unknown-linux-gnu.zip -O /tmp/catp.zip
+    sudo unzip -o /tmp/catp.zip -d /usr/local/bin/ && sudo chmod +x /usr/local/bin/catp && rm /tmp/catp.zip
+}
+
+# Function to install CURL-IMPERSONATE
+install_curl_impersonate() {
+    sudo pacman -Sy --needed nss ca-certificates wget
+    wget https://github.com/lwthiker/curl-impersonate/releases/download/v0.6.0-alpha.1/curl-impersonate-v0.6.0-alpha.1.aarch64-linux-gnu.tar.gz -O /usr/local/bin/curl-imp.tar.gz
+    sudo tar -xvf /usr/local/bin/curl-imp.tar.gz -C /usr/local/bin && sudo chmod +x /usr/local/bin/curl* && sudo rm /usr/local/bin/curl-imp.tar.gz
+}
+
+# Function to install NTFY
+install_ntfy() {
+    install_yay
+    yay -S ntfysh-bin
+}
+
+# Function to install JQP
+install_jqp() {
+    install_yay
+    yay -S jqp-bin
+}
+
+# Function to install K9S
+install_k9s() {
+    sudo pacman -S k9s
+}
+
+# Function to install NEOFETCH
+install_neofetch() {
+    sudo pacman -S neofetch
+}
+
+# Function to install OTS
+install_ots() {
+    if ! command -v go &> /dev/null; then
+        sudo pacman -Sy --needed go
+    fi
+    go get -u github.com/sniptt-official/ots
+}
+
+# Function to install BOTTOM
+install_bottom() {
+    sudo pacman -S bottom
+}
+
+# Function to install VIDDY
+install_viddy() {
+    install_yay
+    yay -S viddy
+}
+
+# Function to install HTMLQ
+install_htmlq() {
+    if ! command -v cargo &> /dev/null; then
+        sudo pacman -Sy --needed rust
+    fi
+    cargo install htmlq
+}
+
+# Function to install SYSZ
+install_sysz() {
+    install_yay
+    yay -S sysz
+}
+
+# Function to install PUEUE
+install_pueue() {
+    sudo pacman -S pueue
+}
+
+# Function to install VIZEX
+install_vizex() {
+    install_yay
+    yay -S vizex
+}
+
+# Function to install GPING
+install_gping() {
+    sudo pacman -S gping
+}
+
+# Function to install TEMPMAIL
+install_tempmail() {
+    install_yay
+    yay -S tmpmail-git
+}
+
+# Function to install BAT
+install_bat() {
+    sudo pacman -S bat
+}
+
+# Function to install NAVI
+install_navi() {
+    sudo pacman -S navi
+}
+
+echo "Ricing base install"
+sudo pacman -S nerd-fonts zsh fasd peco acpi
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+git clone https://github.com/jimeh/zsh-peco-history.git $ZSH_CUSTOM/plugins/zsh-peco-history
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/MichaelAquilina/zsh-you-should-use.git $ZSH_CUSTOM/plugins/you-should-use
+sed -i '/^ZSH_THEME=/c\ZSH_THEME="spaceship"' ~/.zshrc
+sed -i '/^plugins=(/c\plugins=(git fasd zsh-peco-history zsh-autosuggestions zsh-syntax-highlighting you-should-use docker archlinux ansible aws azure battery colored-man-pages colorize command-not-found docker-compose rsync rust terraform zsh-interactive-cd)' ~/.zshrc
+echo "alias cdi='cd \$(ls | peco)'" >> ~/.zshrc
+echo "alias -g xcopy='xclip -selection clipboard'" >> ~/.zshrc
+echo "alias -g xpaste='xclip -selection clipboard -o'" >> ~/.zshrc
+mkdir -p ~/.config && touch ~/.config/spaceship.zsh
+
+cat << "EOF" > ~/.config/spaceship.zsh
+SPACESHIP_PROMPT_ORDER=(
+  time          # Time stamps section
+  user          # Username section
+  dir           # Current directory section
+  host          # Hostname section
+  git           # Git section (git_branch + git_status)
+  python
+  hg            # Mercurial section (hg_branch  + hg_status)
+  exec_time     # Execution time
+  line_sep      # Line break
+  battery       # Battery level and status
+  jobs          # Background jobs indicator
+  exit_code     # Exit code section
+  sudo          # Sudo indicator
+  char          # Prompt character
+)
+
+SPACESHIP_TIME_SHOW=true
+SPACESHIP_USER_SHOW=always
+SPACESHIP_PROMPT_ADD_NEWLINE=false
+SPACESHIP_CHAR_SYMBOL="❯"
+SPACESHIP_CHAR_SUFFIX=" "
+SPACESHIP_DIR_COLOR=red
+SPACESHIP_USER_COLOR=red
+SPACESHIP_TIME_FORMAT='%D{%H:%M}'
+SPACESHIP_TIME_COLOR=red
+EOF
+
+#User Choices process
+for choice in $choices
+do
+    case $choice in
+        1) install_yay ;;
+        2) install_hunt ;;
+        3) install_dooit ;;
+        4) install_plow ;;
+        5) install_catp ;;
+        6) install_curl_impersonate ;;
+        7) install_ntfy ;;
+        8) install_jqp ;;
+        9) install_k9s ;;
+        10) sudo pacman -S nnn ;;
+        11) sudo pacman -S ncdu ;;
+        12) sudo pacman -S ssh-tools ;;
+        13) echo "Nala is only for APT (Debian/Ubuntu systems)." ;;
+        14) install_neofetch ;;
+        15) install_ots ;;
+        16) install_bottom ;;
+        17) install_viddy ;;
+        18) install_htmlq ;;
+        19) install_sysz ;;
+        21) install_pueue ;;
+        22) install_vizex ;;
+        23) install_gping ;;
+        24) install_tempmail ;;
+        25) install_bat ;;
+        26) install_navi ;;
+        27) # Installation de tous les outils
+            install_yay
+            install_hunt
+            install_dooit
+            install_plow
+            install_catp
+            install_curl_impersonate
+            install_ntfy
+            install_jqp
+            install_k9s
+            sudo pacman -S nnn
+            sudo pacman -S ncdu
+            sudo pacman -S ssh-tools
+            install_bottom
+            install_neofetch
+            install_ots
+            install_viddy
+            install_htmlq
+            install_sysz
+            sudo pacman -S pueue
+            install_vizex
+            sudo pacman -S gping
+            install_tempmail
+            sudo pacman -S bat
+            sudo pacman -S navi
+            # ... (Ajoutez d'autres commandes d'installation ici)
+            ;;
+        *) echo "Invalid choice: $choice" ;;
+    esac
+done
+
+echo "Installation complete."
